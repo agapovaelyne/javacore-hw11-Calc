94ed4ed6286745a4538fc8d9e7059d9600d3426b
agapovaelyne <agapova.lina@gmail.com>
2021-06-12 23:35:03 +0300

    Format corrections

--- src/main/java/IntsCalculator.java
+++ src/main/java/IntsCalculator.java
@@ -6,29 +6,29 @@ public class IntsCalculator implements Ints {
     }
 
     @Override
-    public int sum(int arg0, int arg1){
+    public int sum(int arg0, int arg1) {
         return (int) target.newFormula()
                 .addOperand(arg0)
                 .addOperand(arg1)
                 .calculate(Calculator.Operation.SUM)
                 .result();
-    };
+    }
 
     @Override
-    public int mult(int arg0, int arg1){
+    public int mult(int arg0, int arg1) {
         return (int) target.newFormula()
                 .addOperand(arg0)
                 .addOperand(arg1)
                 .calculate(Calculator.Operation.MULT)
                 .result();
-    };
+    }
 
     @Override
-    public int pow(int a, int b){
+    public int pow(int a, int b) {
         return (int) target.newFormula()
                 .addOperand(a)
                 .addOperand(b)
                 .calculate(Calculator.Operation.POW)
                 .result();
-    };
+    }
 }
